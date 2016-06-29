04e7558833950c60c79f93c73c31cae798816b15
Luis Alberto <lalberto8085@gmail.com>
2016-06-29 15:38:56 -0400

    adding AllEventPropertiesJsonLayoutRenderer

---
+++ src/NLog.EventPropertiesFormaters/AllEventPropertiesJsonLayoutRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NLog.LayoutRenderers;
+
+namespace NLog.EventPropertiesFormaters
+{
+    [LayoutRenderer("all-event-properties-json")]
+    public class AllEventPropertiesJsonLayoutRenderer : LayoutRenderer
+    {
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var serializedString = logEvent.Properties == null || !logEvent.Properties.Any()
+                ? null
+                : JsonConvert.SerializeObject(logEvent.Properties);
+
+            builder.Append(serializedString);
+        }
+    }
+}
